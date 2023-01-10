ed7a3c0e0b7e78178eaeb11cecea4d1ebf10f087
Dmitriy Volkanov <105171736+Krendel517@users.noreply.github.com>
2023-01-10 18:40:20 +0200

    Change font color for hint

--- ClearBackground/Form1.Designer.cs
+++ ClearBackground/Form1.Designer.cs
@@ -106,7 +106,7 @@ namespace ClearBackground
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.ForeColor = System.Drawing.Color.Black;
             this.label3.Location = new System.Drawing.Point(12, 120);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(354, 16);
