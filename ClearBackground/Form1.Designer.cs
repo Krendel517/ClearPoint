7c58df77d3a9679f55f9dc23cd2b54538b756083
AntonAntipov29 <antipovhuman@gmail.com>
2023-01-16 23:49:30 +0200

    Improved form view

--- ClearBackground/Form1.Designer.cs
+++ ClearBackground/Form1.Designer.cs
@@ -37,36 +37,39 @@ namespace ClearBackground
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAddPath = new System.Windows.Forms.Button();
+            this.checkPath = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // ReadFile
             // 
             this.ReadFile.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ReadFile.Location = new System.Drawing.Point(422, 130);
+            this.ReadFile.Location = new System.Drawing.Point(422, 179);
             this.ReadFile.Name = "ReadFile";
             this.ReadFile.Size = new System.Drawing.Size(125, 50);
             this.ReadFile.TabIndex = 0;
-            this.ReadFile.Text = "ReadFile";
+            this.ReadFile.Text = "Start";
             this.ReadFile.UseVisualStyleBackColor = true;
             this.ReadFile.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtPointPath
             // 
-            this.txtPointPath.Location = new System.Drawing.Point(12, 38);
+            this.txtPointPath.Location = new System.Drawing.Point(12, 23);
             this.txtPointPath.Name = "txtPointPath";
             this.txtPointPath.Size = new System.Drawing.Size(437, 20);
             this.txtPointPath.TabIndex = 1;
             // 
             // txtPolygonPath
             // 
-            this.txtPolygonPath.Location = new System.Drawing.Point(12, 86);
+            this.txtPolygonPath.Location = new System.Drawing.Point(12, 71);
             this.txtPolygonPath.Name = "txtPolygonPath";
             this.txtPolygonPath.Size = new System.Drawing.Size(437, 20);
             this.txtPolygonPath.TabIndex = 2;
             // 
             // btnOpenPoint
             // 
-            this.btnOpenPoint.Location = new System.Drawing.Point(455, 33);
+            this.btnOpenPoint.Location = new System.Drawing.Point(455, 18);
             this.btnOpenPoint.Name = "btnOpenPoint";
             this.btnOpenPoint.Size = new System.Drawing.Size(92, 28);
             this.btnOpenPoint.TabIndex = 3;
@@ -76,7 +79,7 @@ namespace ClearBackground
             // 
             // btnOpenPolygon
             // 
-            this.btnOpenPolygon.Location = new System.Drawing.Point(455, 78);
+            this.btnOpenPolygon.Location = new System.Drawing.Point(455, 66);
             this.btnOpenPolygon.Name = "btnOpenPolygon";
             this.btnOpenPolygon.Size = new System.Drawing.Size(92, 28);
             this.btnOpenPolygon.TabIndex = 4;
@@ -87,7 +90,7 @@ namespace ClearBackground
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Location = new System.Drawing.Point(12, 7);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(130, 13);
             this.label1.TabIndex = 5;
@@ -96,29 +99,59 @@ namespace ClearBackground
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Location = new System.Drawing.Point(12, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.Size = new System.Drawing.Size(144, 13);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Path to polygon coordinates";
+            this.label2.Text = "Path to polygons coordinates";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Location = new System.Drawing.Point(12, 163);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(354, 16);
+            this.label3.Size = new System.Drawing.Size(353, 16);
             this.label3.TabIndex = 7;
             this.label3.Text = "Attention! The coordinates must be in the correct sequence";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Add path for result file";
+            // 
+            // btnAddPath
+            // 
+            this.btnAddPath.Location = new System.Drawing.Point(455, 114);
+            this.btnAddPath.Name = "btnAddPath";
+            this.btnAddPath.Size = new System.Drawing.Size(92, 28);
+            this.btnAddPath.TabIndex = 9;
+            this.btnAddPath.Text = "Open Path";
+            this.btnAddPath.UseVisualStyleBackColor = true;
+            this.btnAddPath.Click += new System.EventHandler(this.btnAddPath_Click);
+            // 
+            // checkPath
+            // 
+            this.checkPath.Location = new System.Drawing.Point(12, 119);
+            this.checkPath.Name = "checkPath";
+            this.checkPath.Size = new System.Drawing.Size(437, 20);
+            this.checkPath.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(559, 193);
+            this.ClientSize = new System.Drawing.Size(559, 241);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnAddPath);
+            this.Controls.Add(this.checkPath);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -129,6 +162,7 @@ namespace ClearBackground
             this.Controls.Add(this.ReadFile);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,6 +178,9 @@ namespace ClearBackground
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAddPath;
+        private System.Windows.Forms.TextBox checkPath;
     }
 }
 
