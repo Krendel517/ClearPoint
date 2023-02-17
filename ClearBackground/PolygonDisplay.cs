780e29997b36899574f0133aff2a9aa34bf8160f
ct1kep <sashapavlenko1996@gmail.com>
2023-02-18 00:24:30 +0200

    Added sorting of polygon by angles

--- ClearBackground/PolygonDisplay.cs
+++ ClearBackground/PolygonDisplay.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 
 namespace ClearBackground
 {
@@ -28,6 +29,7 @@ namespace ClearBackground
             YReductionToCoordinateSystem();
             CenteringPolygon();
             StretchingPolygon();
+            ArrangementOfPoints();
         }
 
         private void SetPolygon(PointD[] polygon)
@@ -174,6 +176,46 @@ namespace ClearBackground
                     vectorCoordinateY = polygonPoints[i].Y - centerOfPicturebox.Y;
                     polygonPoints[i].X = polygonPoints[i].X + vectorCoordinateX * (halfSizeDifferenceY - preventingOverscale);
                     polygonPoints[i].Y = polygonPoints[i].Y + vectorCoordinateY * (halfSizeDifferenceY - preventingOverscale);
+                }
+            }
+        }
+
+        private void ArrangementOfPoints()
+        {
+            double[] angleFromCentre = new double[polygonPoints.Length];
+            double angleForSorting;
+            double lineDistance;
+            PointF pointForSorting;
+
+            for (int i = 0; i < polygonPoints.Length; i++)
+            {
+                double xOfLine = polygonPoints[i].X - centerOfPicturebox.X;
+                double squareDegreeOfX = xOfLine * xOfLine;
+                double yOfLine = polygonPoints[i].Y - centerOfPicturebox.Y;
+                double squareDegreeOfY = yOfLine * yOfLine;
+                lineDistance = Math.Sqrt(squareDegreeOfX + squareDegreeOfY);
+                angleFromCentre[i] = Math.Acos((polygonPoints[i].X - centerOfPicturebox.X) / lineDistance);
+
+                if (polygonPoints[i].Y < centerOfPicturebox.Y)
+                {
+                    angleFromCentre[i] = 2 * Math.PI - angleFromCentre[i];
+                }
+            }
+
+            for (int i = 0; i < polygonPoints.Length - 1; i++)
+            {
+                for (int j = i + 1; j < polygonPoints.Length; j++)
+                {
+                    if (angleFromCentre[i] > angleFromCentre[j])
+                    {
+                        angleForSorting = angleFromCentre[i];
+                        angleFromCentre[i] = angleFromCentre[j];
+                        angleFromCentre[j] = angleForSorting;
+
+                        pointForSorting = polygonPoints[i];
+                        polygonPoints[i] = polygonPoints[j];
+                        polygonPoints[j] = pointForSorting;
+                    }
                 }
             }
         }
